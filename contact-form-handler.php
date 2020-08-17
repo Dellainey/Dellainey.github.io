556d2bc23b00f2c23c4cde376cc0933b930d2524
Dellainey <dellainey.alcacoas@gmail.com>
2020-08-17 21:59:25 +0200

    Initial Commit

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,23 +1,11 @@
 <?php 
-$errors = '';
-$myemail = 'dellainey.alcacoas@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
+
+$myemail = 'dellainey.alcacoas@gmail.com';
+
 
 $name = $_POST['name']; 
 $email_address = $_POST['email']; 
 $message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
 
 if( empty($errors))
 {
@@ -34,18 +22,3 @@ if( empty($errors))
 	header('Location: contact-form-thank-you.html');
 } 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-
-
-</body>
-</html>
